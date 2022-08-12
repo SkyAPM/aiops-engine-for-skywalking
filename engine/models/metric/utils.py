809ec3b29b6c0f593cd9523e48404ac4adb14870
Fengrui-Liu <liufengrui18z@ict.ac.cn>
2022-08-12 22:05:52 +0800

    Func: alert manager

--- engine/models/metric/utils.py
+++ engine/models/metric/utils.py
@@ -1,3 +1,5 @@
+from collections import deque
+import numpy as np
 class AlertManager:
     def __init__(self, tolearance: int = 5, least_alert: int = 2) -> None:
         self.tolearance = tolearance
@@ -5,12 +7,26 @@ class AlertManager:
         self.alert_cnt = 0
         self.non_alert_cnt = 0
         self.is_alerting = False
+        self.filter = deque(maxlen=20)
 
-    def get_alert(self, timestamp, score: float):
+    def get_alert(self, timestamp, X, score: float):
         if score is None:
             return None
         assert type(score) is float or type(score) is int, "Score must be float"
         assert 0 <= score <= 1, "Score must be between 0 and 1"
+
+        if score == 1.0:
+            if len(self.filter) > 4:
+                filters = {idx:abs(X-i) for idx, i in enumerate(self.filter)}
+                filters = dict(sorted(filters.items(), key= lambda x: x[1]))
+
+                refs = np.array(self.filter)[np.array(list(filters.keys())[:10])]
+                refs_mean = np.mean(refs)
+                refs_std = np.std(refs)
+                res_score = abs(X - refs_mean) / refs_std if refs_std !=0 else abs(X - refs_mean)
+                if res_score < 2:
+                    score = 0.5
+            self.filter.append(X)
 
         if score == 1.0:
             self.alert_cnt += 1
@@ -32,6 +48,8 @@ class AlertManager:
                 self.is_alerting = False
 
             elif self.is_alerting:
-                score = 1.0
+                # This can be set for normal data to alerts.
+                # score = 1.0
+                score = score
 
         return score
