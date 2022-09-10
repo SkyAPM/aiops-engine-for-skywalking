6cea137525380b7ad5d2cae25c1ee2e81b9efee7
Superskyyy (OFFLINE) <Superskyyy@outlook.com>
2022-09-09 22:36:03 -0400

    Add CI, linter and license checkers
    1. Add initial CI to cover lint, license check, DLC
    2. Enforce coding style
    3. Update dependency and provide commands to fix code smell

--- engine/models/metric/base/detector.py
+++ engine/models/metric/base/detector.py
@@ -1,37 +1,83 @@
+#  Copyright 2022 SkyAPM org
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
 from abc import ABC, abstractmethod
 from typing import Union
-from datetime import datetime
-from dateutil.parser import parse
-import numpy as np
+
 
 class BaseDetector(ABC):
-    """Abstract class for Detector, supporting for customize detector."""
+    """Abstract class for Detector, supporting for customize detectors."""
 
     def __init__(self):
         """Initialization BaseDetector"""
-        self.data_type = "multivariate"
+        self.data_type = 'multivariate'
         self.index = -1
         self.window_len = 100
 
     def _check(self, timestamp: str, X: Union[float, int]) -> bool:
-        """Check whether the detector can handle the data."""
+        """Check whether the detectors can handle the data."""
 
         assert type(X) in [
             float,
             int
-        ], "Please input X with float or int type."
+        ], 'Please input X with float or int type.'
         assert type(timestamp)
 
         self.index += 1
 
     @abstractmethod
     def fit(self, X: float):
-
         return NotImplementedError
 
     @abstractmethod
     def score(self, X: float) -> float:
-
         return NotImplementedError
 
     def fit_score(self, timestamp: str, X: Union[float, int]) -> float:
