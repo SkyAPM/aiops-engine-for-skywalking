410211cb1a0c8bf50b635030140787d35882a9af
liangshumin <liangshumin16@163.com>
2022-09-16 21:59:31 +0800

    Add cluster and make linter check

---
+++ experiments/log/clustering/drain3/memory_buffer_persistence.py
@@ -0,0 +1,14 @@
+# SPDX-License-Identifier: MIT
+
+from drain3.persistence_handler import PersistenceHandler
+
+
+class MemoryBufferPersistence(PersistenceHandler):
+    def __init__(self):
+        self.state = None
+
+    def save_state(self, state):
+        self.state = state
+
+    def load_state(self):
+        return self.state
