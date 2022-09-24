bde41c6f0d9674c514b50773d6517447159b73fa
Superskyyy <superskyyy@outlook.com>
2022-09-24 13:39:40 -0400

    Add conf manager and update missing bit
    Signed-off-by: Superskyyy <superskyyy@outlook.com>

--- config.py
+++ config.py
@@ -12,18 +12,12 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-.idea/
-*.iml
-.DS_Store
-*~
-__pycache__
-build/
-dist/
-*.pyc
-**/*.egg-info/
-**/*venv*/
-.vscode/
-engine_protocol/
-assets/datasets
-# Ignore dynaconf secret files
-.secrets.*
+from dynaconf import Dynaconf
+
+settings = Dynaconf(
+    envvar_prefix="DYNACONF",
+    settings_files=['settings.toml', '.secrets.toml'],
+)
+
+# `envvar_prefix` = export envvars with `export DYNACONF_FOO=bar`.
+# `settings_files` = Load these files in the order.
