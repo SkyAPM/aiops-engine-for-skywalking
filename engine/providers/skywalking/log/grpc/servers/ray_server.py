d0551a75fc16a0e1957a496c770315d27fcb7a36
Superskyyy <superskyyy@outlook.com>
2022-09-27 02:10:02 -0400

    Fix rpc server serve function
    Signed-off-by: Superskyyy <superskyyy@outlook.com>

--- engine/providers/skywalking/log/grpc/servers/ray_server.py
+++ engine/providers/skywalking/log/grpc/servers/ray_server.py
@@ -44,28 +44,35 @@ from redis import asyncio as aioredis  # noqa
 
 
 @ray.remote(num_cpus=1)
-class GRPCIngestorActor:
+class GRPCIngestorActor():
     def __init__(self, redis_info, bind_address):
         self.run = None
         self.bind_address = bind_address
         self.redis_info = redis_info
+        options = (('grpc.so_reuseport', 1),)
+        self.server = grpc.aio.server(options=options)
 
     async def _run_server(self):
-        """Start a server in a subprocess."""
+        """
+        Starts an Async gRPC server, port binding to more than
+        one process will only work on Unix systems.
+        On unsupported systems, this will fall back to a single
+        actor server.
+        """
         _LOGGER.info('Starting new server.')
-        options = (('grpc.so_reuseport', 1),)
-        server = grpc.aio.server(options=options)
-        redis_conn = await aioredis.from_url(
+
+        self.redis_conn = await aioredis.from_url(
             f'redis://{self.redis_info["REDIS_HOSTNAME"]}:{self.redis_info["REDIS_PORT"]}',
             retry_on_timeout=True, username=self.redis_info['username'],
             password=self.redis_info['password'])
 
-        self.redis_conn = redis_conn
-        log_exporter_pb2_grpc.add_LogExportServiceServicer_to_server(LogIngestorServicer(redis_conn=redis_conn),
-                                                                     server)
-        server.add_insecure_port(self.bind_address)
-        await server.start()
-        await server.wait_for_termination()
+        log_exporter_pb2_grpc.add_LogExportServiceServicer_to_server(
+            LogIngestorServicer(redis_conn=self.redis_conn),
+            self.server,
+        )
+        self.server.add_insecure_port(self.bind_address)
+        await self.server.start()
+        await self.server.wait_for_termination()
 
     async def start(self):
         await self._run_server()
@@ -76,55 +83,57 @@ class GRPCIngestorActor:
     def destroy(self):
         self.run = False
 
-    def _wait_forever(self, server):
+    def _wait_forever(self):
         try:
             while True:
                 time.sleep(_ONE_DAY.total_seconds())
         except KeyboardInterrupt:
-            server.stop(None)
+            self.server.stop(None)
 
 
 @contextlib.contextmanager
 def _reserve_port():
     """Find and reserve a port for all subprocesses to use."""
     sock = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
-
+    supported = True
     try:
         sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
-        if sock.getsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT) == 0:
-            raise RuntimeError("Failed to set SO_REUSEPORT.")
-    except Exception as e:
-        print('SO_REUSEPORT not supported')
-        print(e)
+    except AttributeError as e:
+        supported = False
+        _LOGGER.info('SO_REUSEPORT not supported on this platform, falling back to single process')
+        _LOGGER.info(e)
 
     sock.bind(('', 50051))  # 0 means to select an arbitrary unused port
     try:
-        yield sock.getsockname()[1]
+        yield sock.getsockname()[1], supported
     finally:
         sock.close()
 
 
 def main():
-    with _reserve_port() as port:
+    with _reserve_port() as (port, success):
+        _LOGGER.info(f'Port {port}, success {success}')
         bind_address = 'localhost:{}'.format(port)
         _LOGGER.info("Binding to '%s'", bind_address)
         sys.stdout.flush()
-
         # NOTE: It is imperative that the worker subprocesses be forked before
         # any gRPC servers start up. See
         # https://github.com/grpc/grpc/issues/16001 for more details.
+        if not success:
+            grpc_ingestors = [GRPCIngestorActor.remote(secrets_temp.redis_info, bind_address) for _ in range(1)]
+        else:
+            grpc_ingestors = [GRPCIngestorActor.remote(secrets_temp.redis_info, bind_address) for _ in range(2)]
 
         # make count of actor configurable
-        consumers = [GRPCIngestorActor.remote(secrets_temp.redis_info, bind_address) for _ in range(1)]
         try:
-            refs = [consumer.start.remote() for consumer in consumers]
+            refs = [grpc_ingestor.start.remote() for grpc_ingestor in grpc_ingestors]
             ray.get(refs)
         except KeyboardInterrupt:
-            for c in consumers:
-                c.stop.remote()
+            for grpc_ingestor in grpc_ingestors:
+                grpc_ingestor.stop.remote()
         finally:
-            for c in consumers:
-                c.destroy.remote()
+            for grpc_ingestor in grpc_ingestors:
+                grpc_ingestor.destroy.remote()
 
 
 if __name__ == '__main__':
